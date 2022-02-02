438fa7bb6289c9ecc12b08e0f90866dda0ee19b8
alexsmaldone <alexsmaldone@gmail.com>
2022-02-02 18:29:18 -0500

    first commit

---
+++ backend/routes/index.js
@@ -0,0 +1,11 @@
+// backend/routes/index.js
+const csurf = require("csurf");
+const express = require("express");
+const router = express.Router();
+
+router.get("/hello/world", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  res.send("Hello World!");
+});
+
+module.exports = router;
