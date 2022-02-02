61bb66042f32283e352c6109bf9dfd6d01c4fed1
alexsmaldone <alexsmaldone@gmail.com>
2022-02-02 18:37:10 -0500

    finished phase 1 api

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,9 @@
 const csurf = require("csurf");
 const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
 
 router.get("/hello/world", function (req, res) {
   res.cookie("XSRF-TOKEN", req.csrfToken());
