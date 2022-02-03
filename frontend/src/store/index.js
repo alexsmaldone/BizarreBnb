9b6f9a8d25e8cff6205b7edd645ad0ce0db633aa
alexsmaldone <alexsmaldone@gmail.com>
2022-02-03 12:45:06 -0500

    loginform

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import * as sessionActions from "./session";
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
-  // add reducer functions here
+  session: sessionReducer,
 });
 
 let enhancer;
