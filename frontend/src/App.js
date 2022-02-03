60f5db855bf93f05503b349f52b3cbc4ac428591
alexsmaldone <alexsmaldone@gmail.com>
2022-02-03 12:50:19 -0500

    loginform page and component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
+// frontend/src/App.js
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
-  return <h1>Hello from App</h1>;
+  return (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
