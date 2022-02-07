0c84d32aa7672f0c54a2dc166884c537d7ca661b
alexsmaldone <alexsmaldone@gmail.com>
2022-02-06 23:15:35 -0500

    splash background image object fit

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -6,7 +6,10 @@ import "./HomePage.css";
 const splashPage = () => {
   return (
     <main>
-      <h1>Welcome to BizarreBnb</h1>
+      <div className="background-image-container">
+        <img src="https://a0.muscache.com/im/pictures/9b731de6-b8b6-4eae-aba2-631216bf1bfc.jpg?im_w=1200" />
+      </div>
+      <h1 className="welcome">Welcome to BizarreBnb</h1>
     </main>
   );
 };
