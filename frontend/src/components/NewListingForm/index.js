03c2de30acb362b5d1ecd350835bd9a131c1445e
alexsmaldone <alexsmaldone@gmail.com>
2022-02-08 12:21:34 -0500

    created new listing form

--- frontend/src/components/NewListingForm/index.js
+++ frontend/src/components/NewListingForm/index.js
@@ -1,13 +1,145 @@
 import React, { useState, useEffect } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import * as sessionActions from "../../store/session";
+import { createListing } from "../../store/listing";
 
 import "./NewListingForm.css";
 
 function NewListingForm() {
   const dispatch = useDispatch();
-  return null;
+  const history = useHistory();
+
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipcode, setZipcode] = useState("");
+  const [country, setCountry] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [guests, setGuests] = useState("");
+  const [bedroom, setBedroom] = useState("");
+  const [bath, setBath] = useState("");
+
+  const updateAddress = (e) => setAddress(e.target.value);
+  const updateCity = (e) => setCity(e.target.value);
+  const updateState = (e) => setState(e.target.value);
+  const updateZipcode = (e) => setZipcode(e.target.value);
+  const updateCountry = (e) => setCountry(e.target.value);
+  const updateName = (e) => setName(e.target.value);
+  const updateDescription = (e) => setDescription(e.target.value);
+  const updatePrice = (e) => setPrice(e.target.value);
+  const updateGuests = (e) => setGuests(e.target.value);
+  const updateBedroom = (e) => setBedroom(e.target.value);
+  const updateBath = (e) => setBath(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const payload = {
+      address,
+      city,
+      state,
+      zipcode,
+      country,
+      name,
+      description,
+      price,
+      guests,
+      bedroom,
+      bath,
+    };
+
+    const listing = await dispatch(createListing(payload));
+    if (listing) {
+      history.push(`listings/${listing.id}`);
+    }
+  };
+
+  return (
+    <main>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Address - '1122 Boogie Woogie Ave'"
+            required
+            value={address}
+            onChange={updateAddress}
+          />
+          <input
+            type="text"
+            placeholder="City"
+            required
+            value={city}
+            onChange={updateCity}
+          />
+          <input
+            type="text"
+            placeholder="State"
+            value={state}
+            onChange={updateState}
+          />
+          <input
+            type="text"
+            placeholder="Zipcode"
+            required
+            value={zipcode}
+            onChange={updateZipcode}
+          />
+          <input
+            type="text"
+            placeholder="Country"
+            required
+            value={country}
+            onChange={updateCountry}
+          />
+          <input
+            type="text"
+            placeholder="Listing Title"
+            required
+            value={name}
+            onChange={updateName}
+          />
+          <textarea
+            type="text"
+            placeholder="Description of listing..."
+            required
+            value={description}
+            onChange={updateDescription}
+          />
+          <input
+            type="number"
+            placeholder="Price per night"
+            required
+            value={price}
+            onChange={updatePrice}
+          />
+          <input
+            type="number"
+            placeholder="Number of Guests"
+            required
+            value={guests}
+            onChange={updateGuests}
+          />
+          <input
+            type="number"
+            placeholder="Number of Bedrooms"
+            required
+            value={bedroom}
+            onChange={updateBedroom}
+          />
+          <input
+            type="number"
+            placeholder="Number of bathrooms"
+            required
+            value={bath}
+            onChange={updateBath}
+          />
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default NewListingForm;
